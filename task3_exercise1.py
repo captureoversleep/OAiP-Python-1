8736ff2708ca077552691e44557342f07ace82a5
captureoversleep <asdweer@list.ru>
2026-09-05 14:07:39 +0500

    Add files via upload
    task 3
    exercise 1

---
+++ task3_exercise1.py
@@ -0,0 +1,8 @@
+price_weekday = 2100
+price_weekend = 2850
+total_weekday = 4
+total_weekend = 4
+total_price = 0
+
+total_price += (price_weekday * total_weekday) + (price_weekend * total_weekend)
+print(total_price)
