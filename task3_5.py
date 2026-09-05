de02bf0485077f46ae117eba3af19d37e682485f
captureoversleep <asdweer@list.ru>
2026-09-05 18:17:51 +0500

    Add files via upload
    Task 3.5

---
+++ task3_5.py
@@ -0,0 +1,21 @@
+total = int(input('Enter money amount: '))
+if total < 0:
+	print('Error: money amount cannot be negative!')
+elif total == 0:
+	print('Money amount is 0 - no need to solve!')
+else:
+	bank_th = total // 1000
+	total = total % 1000
+	
+	bank_hun = total // 100
+	total = total % 100
+	
+	coin_ten = total // 10
+	total = total % 10
+	
+	coin_one = total
+	
+	print('1000:', bank_th)
+	print('100:', bank_hun)
+	print('10:', coin_ten)
+	print('1:', coin_one)
