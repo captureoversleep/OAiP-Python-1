4ad2a0ce3a90e0d9977540d52b9d99ca05742967
captureoversleep <asdweer@list.ru>
2026-09-05 18:34:17 +0500

    Add files via upload
    Task 3.6

---
+++ task3_6.py
@@ -0,0 +1,6 @@
+big_baggage = int(input('Enter main baggage weight: '))
+handy_bag = int(input('Enter handy bag weight: '))
+additional_handy = int(input('Enter additional handy weight: '))
+
+total_weight = big_baggage + handy_bag + additional_handy
+print('Total weight:', total_weight)
